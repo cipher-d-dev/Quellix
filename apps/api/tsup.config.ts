de7507c2e1653a4705f47c50cfc3b9a3a2533638
Favour Ejiofor <faveejiofor2009@gmail.com>
2026-03-20 02:22:39 +0100

    chore: rewrite tsup.config.ts

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,13 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["server/src/index.ts"],
+  format: ["cjs"],
   outDir: "server/dist",
-  format: ["esm"],
-  target: "es2022",
-  platform: "node",
-  sourcemap: true,
   clean: true,
-  splitting: false,
-  dts: false,
   bundle: true,
 });
